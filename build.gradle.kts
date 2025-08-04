b79d1fce0f02b39c7a00467232cd3e0ffd0366b3
shukyurkevin <shukyurkevin@gmail.com>
2025-08-05 02:11:50 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.kevin.planeta"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-web-services")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.springframework.session:spring-session-jdbc")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/kevin/planeta/Mafia/MafiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.planeta.Mafia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
